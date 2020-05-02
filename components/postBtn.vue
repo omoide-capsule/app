7556c096f474a164d022a6c9a3b0970db2a745bf
Yusei Ito <yuseikun@Outlook.jp>
2020-05-03 01:55:32 +0900

    投稿ボタン

---
+++ components/postBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post-button">
+    投稿
+  </div>
+</template>
+<style>
+.post-button {
+  margin-top: 30px;
+  background-color: #3b5f8a;
+  color: #ffffff;
+  width: 60%;
+  border-radius: 30px;
+  text-align: center;
+  font-weight: bold;
+  box-shadow: -2px 2px 2px 2px #eeeeee;
+  transition: 1s;
+  margin-right: 20px;
+  margin-left: 20px;
+  line-height: 40px;
+  font-size: 14pt;
+  padding: 5px;
+}
+
+.post-button:hover {
+  box-shadow: -3px 3px 3px 3px #eeeeee;
+  background-color: #3b5f8aef;
+}
+</style>
