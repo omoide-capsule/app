561d8b2746f64ef04caeb962d08af2688cd82169
Yusei Ito <yuseikun@Outlook.jp>
2020-05-03 01:53:26 +0900

    画像入力コンポーネントを作成

---
+++ components/pictureInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-flex xl12>
+      <div v-if="uploaded"></div>
+      <div v-else>
+        <label>
+          <picture-button />
+          <input type="file" ref="fileInput" style="display:none" />
+        </label>
+      </div>
+    </v-flex>
+  </v-container>
+</template>
+<script>
+import pictureButton from './pictureButton'
+export default {
+  components: {
+    pictureButton
+  },
+  data() {
+    return {
+      content: '',
+      uploaded: false
+    }
+  }
+}
+</script>
