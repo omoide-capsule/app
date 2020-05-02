1879ad6681d7aeb529521c3656d0ceebed2d4e93
Yusei Ito <yuseikun@Outlook.jp>
2020-05-03 01:52:45 +0900

    投稿のキャプションを書く箱を実装

---
+++ components/bodyInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-divider />
+    <v-flex xl12>
+      <textarea
+        v-model="content"
+        placeholder="この思い出について書く🖋"
+        class="omoideBody"
+      ></textarea>
+    </v-flex>
+    <v-divider />
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      content: ''
+    }
+  }
+}
+</script>
+<style>
+.omoideBody {
+  padding: 10px;
+  width: 100%;
+  height: 200px;
+}
+</style>
