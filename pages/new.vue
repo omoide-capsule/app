00be867eb9a3b0ae01ea00b1a1dab45ceefce4d7
Yusei Ito <yuseikun@Outlook.jp>
2020-05-03 01:52:04 +0900

    新規投稿のページを追加

--- pages/new.vue
+++ pages/new.vue
@@ -1,11 +1,18 @@
 <template>
-  <v-layout column justify-center align-center>
-    <h1>Hello,Tsuioku</h1>
+  <v-layout column align-center>
+    <title-input />
+    <picture-input />
+    <body-input />
+    <post-btn />
   </v-layout>
 </template>
 
 <script>
+import titleInput from '../components/titleInput'
+import bodyInput from '../components/bodyInput'
+import pictureInput from '../components/pictureInput'
+import postBtn from '../components/postBtn'
 export default {
-  components: {}
+  components: { titleInput, pictureInput, bodyInput, postBtn }
 }
 </script>
