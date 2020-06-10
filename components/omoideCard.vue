e543c5d1280ec9ea44c9474ee887e5c5f90f872c
Yusei Ito <yuseikun@Outlook.jp>
2020-06-10 18:34:42 +0900

    リスト用コンポーネントを自作

---
+++ components/omoideCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <h4>{{ title }}</h4>
+    </v-card-title>
+
+    <div
+      v-if="image !== ''"
+      class="image-box"
+      :style="`background-image:url('${image}')`"
+    ></div>
+
+    <v-card-text>
+      <p>{{ description }}</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="#ea5550" text>削除</v-btn>
+      <v-btn color="green" class="button" @click="$emit('close')">閉じる</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scpoped>
+.card header {
+  border-bottom: #3b5f8a dotted 1px;
+}
+
+.image-box {
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  width: 80%;
+  height: 150px;
+  background-size: cover;
+
+  border-radius: 10px;
+}
+
+.button {
+  color: #fff !important;
+  font-weight: bold;
+}
+</style>
---
+++ components/omoideDialog.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog v-model="dialog">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn v-bind="attrs" icon v-on="on">
+        <v-icon>mdi-dots-horizontal</v-icon>
+      </v-btn>
+    </template>
+    <omoide-card
+      :title="omoide.title"
+      :description="omoide.description"
+      :uuid="omoide.uuid"
+      @close="dialog = false"
+    />
+  </v-dialog>
+</template>
+<script>
+import omoideCard from './omoideCard'
+export default {
+  components: {
+    omoideCard
+  },
+  props: {
+    omoide: {
+      type: Object
+    }
+  },
+  data() {
+    return { dialog: false }
+  }
+}
+</script>
